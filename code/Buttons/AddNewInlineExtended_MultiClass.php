e25d9cc29700ba51c2f97446c54e3000828d8f5a
Mellisa Hankins <mell@milkywaymultimedia.com.au>
2015-11-05 11:18:03 +1100

    FEATURE: Added editable empty string for multi class button

--- code/Buttons/AddNewInlineExtended_MultiClass.php
+++ code/Buttons/AddNewInlineExtended_MultiClass.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Milkyway\SS\GridFieldUtils;
 /**
  * Milkyway Multimedia
  * AddNewInlineExtended.php
@@ -7,11 +7,11 @@
  * @author Mellisa Hankins <mell@milkywaymultimedia.com.au>
  */
 
-namespace Milkyway\SS\GridFieldUtils;
-
 class AddNewInlineExtended_MultiClass extends AddNewInlineExtended
 {
 	public $allowedClasses;
+
+	public $showEmptyString = true;
 
 	private $useAllowedClasses;
 
@@ -71,9 +71,12 @@ class AddNewInlineExtended_MultiClass extends AddNewInlineExtended
 				'',
 				$this->getAllowedClasses($grid)
 			)
-			->setEmptyString(_t('GridFieldExtensions.SELECTTYPETOCREATE', '(Select type to create)'))
 			->addExtraClass('no-change-track ss-gridfield-inline-new-extended--class-selector')
 			;
+
+		if($this->showEmptyString) {
+			$field->setEmptyString($this->showEmptyString !== true ? $this->showEmptyString : _t('GridFieldExtensions.SELECTTYPETOCREATE', '(Select type to create)'));
+		}
 
 		return \ArrayData::create([
 			'Title' => $this->getTitle(),
